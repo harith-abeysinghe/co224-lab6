9549021e897f1180c14d65c198fe1978c9cc99a8
Harith Abeysinghe <e19003@eng.pdn.ac.lk>
2023-06-14 09:27:11 +0530

    Correcting code

--- Part 1/CPU.v
+++ Part 1/CPU.v
@@ -275,54 +275,54 @@ module cpu(PC,INSTRUCTION,CLK,RESET,BUSYWAIT, READMEM, WRITEMEM, ADDRESS, WRITED
 
 			//Load word
 			8'b00001110:	begin
-								assign WRITE = 1;   		//Trigerring the write enable
-								assign signSelect = 0;    		//Selecting the positive number
-								assign immSelect = 0;   		//Selecting the immediate operand
-								assign ALUOP = 3'b101;		//selecting the ADD operation from ALU
-								assign BJSelect = 2'b00; 	//normal flow
-								assign MEMREAD = 1;			//assigning the memread into 1
-								assign MEMWRITE = 0;
-								assign MUX3 = 1;			//selecting the readdata value into the register file
-								assign SEL_PC = 2'b00;
+								WRITEENABLE = 1'b1;   		//Trigerring the write enable
+								signSelect = 0;    		//Selecting the positive number
+								immSelect = 0;   		//Selecting the immediate operand
+								ALUOP = 3'b101;		//selecting the ADD operation from ALU
+								BJSelect = 2'b00; 	//normal flow
+								READMEM = 1;			//assigning the READMEM into 1
+								WRITEMEM = 0;
+								MUX3 = 1;			//selecting the readdata value into the register file
+								SEL_PC = 2'b00;
 							end
 							
 
 
 			//Load immediate
 			8'b00001111:	begin
-								assign WRITE = 1;   		//Trigerring the write enable
-								assign signSelect = 0;    		//Selecting the positive number
-								assign immSelect = 0;   		//Selecting the immediate operand
-								assign ALUOP = 3'b101;		//selecting the ADD operation from ALU
-								assign BJSelect = 2'b00; 	//normal flow
-								assign MEMREAD = 1;			//assigning the memread into 1
-								assign MEMWRITE = 0;
-								assign MUX3 = 1;			//selecting the readdata value into the register file
-								assign SEL_PC = 2'b00;
+								WRITEENABLE = 1'b1;   		//Trigerring the write enable
+								signSelect = 0;    		//Selecting the positive number
+								immSelect = 0;   		//Selecting the immediate operand
+								ALUOP = 3'b101;		//selecting the ADD operation from ALU
+								BJSelect = 2'b00; 	//normal flow
+								READMEM = 1;			//assigning the READMEM into 1
+								WRITEMEM = 0;
+								MUX3 = 1;			//selecting the readdata value into the register file
+								SEL_PC = 2'b00;
 							end
 			//Stroe word
 			8'b00010000:	begin
-								assign WRITE = 1;   		//Trigerring the write enable
-								assign signSelect = 0;    		//Selecting the positive number
-								assign immSelect = 0;   		//Selecting the immediate operand
-								assign ALUOP = 3'b101;		//selecting the ADD operation from ALU
-								assign BJSelect = 2'b00; 	//normal flow
-								assign MEMWRITE = 1;		//assigning the memwrite into 1
-								assign MEMREAD = 0;
-								assign MUX3 = 0;			//selecting the aluresult value into the register file
-								assign SEL_PC = 2'b00;
+								WRITEENABLE = 1'b1;   		//Trigerring the write enable
+								signSelect = 0;    		//Selecting the positive number
+								immSelect = 0;   		//Selecting the immediate operand
+								ALUOP = 3'b101;		//selecting the ADD operation from ALU
+								BJSelect = 2'b00; 	//normal flow
+								WRITEMEM = 1;		//assigning the WRITEMEM into 1
+								READMEM = 0;
+								MUX3 = 0;			//selecting the aluresult value into the register file
+								SEL_PC = 2'b00;
 							end
 			//Store immediate
 			8'b00010001:	begin
-								assign WRITE = 1;   		//Trigerring the write enable
-								assign signSelect= 0;    		//Selecting the positive number
-								assign immSelect = 0;   		//Selecting the immediate operand
-								assign ALUOP = 3'b101;		//selecting the ADD operation from ALU
-								assign BJSelect = 2'b00; 	//normal flow
-								assign MEMWRITE = 1;		//assigning the memwrite into 1
-								assign MEMREAD = 0;
-								assign MUX3 = 0;			//selecting the aluresult value into the register file
-								assign SEL_PC = 2'b00;
+								WRITEENABLE = 1'b1;   		//Trigerring the write enable
+								signSelect= 0;    		//Selecting the positive number
+								immSelect = 0;   		//Selecting the immediate operand
+								ALUOP = 3'b101;		//selecting the ADD operation from ALU
+								BJSelect = 2'b00; 	//normal flow
+								WRITEMEM = 1;		//assigning the WRITEMEM into 1
+								READMEM = 0;
+								MUX3 = 0;			//selecting the aluresult value into the register file
+								SEL_PC = 2'b00;
 							end
 		endcase
 		
@@ -333,8 +333,8 @@ always @(BUSYWAIT)
 	begin
 		if(~BUSYWAIT) 
 		begin
-			MEMREAD=0;
-			MEMWRITE=0;
+			READMEM=0;
+			WRITEMEM=0;
 		end
 	end
 
